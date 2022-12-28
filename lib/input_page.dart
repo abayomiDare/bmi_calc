5c6d70ff8a484a15221ebfb402686c8b25f738f4
abayomiDare <01.abayomidare>
2022-12-28 06:18:59 +0100

    added slider theme

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -85,17 +85,26 @@ class _InputPageState extends State<InputPage> {
                         ),
                       ],
                     ),
-                    Slider(
-                        value: height.toDouble(),
-                        min: 120.0,
-                        max: 220.0,
-                        activeColor: const Color(0xFFEB1555),
-                        inactiveColor: const Color(0xFF8D8E98),
-                        onChanged: (newValue) {
-                          setState(() {
-                            height = newValue.round();
-                          });
-                        }),
+                    SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                          activeTrackColor: Colors.white,
+                          inactiveTrackColor: const Color(0xFF8D8E98),
+                          thumbColor: const Color(0xFFEB1555),
+                          overlayColor: const Color(0x29EB1555),
+                          thumbShape: const RoundSliderThumbShape(
+                              enabledThumbRadius: 15.0),
+                          overlayShape: const RoundSliderOverlayShape(
+                              overlayRadius: 30.0)),
+                      child: Slider(
+                          value: height.toDouble(),
+                          min: 120.0,
+                          max: 220.0,
+                          onChanged: (newValue) {
+                            setState(() {
+                              height = newValue.round();
+                            });
+                          }),
+                    ),
                   ],
                 )),
           ),
